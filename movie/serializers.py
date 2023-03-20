c892f2e3c32152f23f702bf109519547fb2c424c
tanaki <tanaki.1609@gmail.com>
2023-03-20 19:58:08 +0600

    lesson 2

--- movie/serializers.py
+++ movie/serializers.py
@@ -1,9 +1,34 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Director, Genre, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'id stars text'.split()
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = 'id name age'.split()
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    # genres = GenreSerializer(many=True)
+    director_str = serializers.SerializerMethodField()
+    filter_reviews = ReviewSerializer(many=True)
+
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = 'id name genre_list director_name director_str filter_reviews'.split()
         # exclude = 'id'.split()
+
+    def get_director_str(self, movie):
+        return movie.director_name
